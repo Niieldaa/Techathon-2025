f3d7c5069cb0ccf7df1e25ee5eeb95e9f5413121
TheCookieLeg <lucas2.olafsen@gmail.com>
2025-10-26 10:49:52 +0100

    IT WORKS! MY TIME MACHINE WORKS

--- TechathonProject/Assets/Scripts/EngravingChiesel.cs
+++ TechathonProject/Assets/Scripts/EngravingChiesel.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,24 +7,31 @@ public class EngravingChisel : MonoBehaviour
     [Header("Engraving Settings")]
     public GameObject engravingPointPrefab;
     public float engravingRange = 0.2f; // optional raycast distance
+    Coroutine currentCoroutine;
+    public float timeBetweenHits = 1;
 
     [SerializeField] GameObject plane;
 
     private bool hammerTouching = false;
     private bool rockTouching = false;
 
+    void Start()
+    {
+        currentCoroutine = null;
+    }
+
     void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Hammer"))
         {
             hammerTouching = true;
-            plane.GetComponent<MeshRenderer>().material.color = Color.red;
+            //plane.GetComponent<MeshRenderer>().material.color = Color.red;
         }
             
         if (collision.CompareTag("Rock"))
         {
             rockTouching = true;
-            plane.GetComponent<MeshRenderer>().enabled = false;
+            //plane.GetComponent<MeshRenderer>().enabled = false;
         }
 
         // Only spawn if both are touching
@@ -31,24 +39,40 @@ public class EngravingChisel : MonoBehaviour
         {
             //SpawnEngraving(collision);
             Debug.Log("Spawn engraving now!");
+            if (currentCoroutine == null)
+            {
+                currentCoroutine = StartCoroutine(SpawnEngravingSimple(collision));
+            }
         }
 
-        SpawnEngravingSimple(collision);
+        
 
     }
-
-    void SpawnEngravingSimple(Collider rock)
+    
+    IEnumerator SpawnEngravingSimple(Collider rock)
     {
         Vector3 point = transform.position;
         Vector3 normal = rock.transform.up;
-        Instantiate(engravingPointPrefab, point + normal * 0.00000001f, Quaternion.LookRotation(normal));
-    
+        Instantiate(engravingPointPrefab, point + normal * 0.0001f, Quaternion.LookRotation(normal));
+        yield return new WaitForSeconds(timeBetweenHits);
+        currentCoroutine = null;
+
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider collision)
     {
-        plane.GetComponent<MeshRenderer>().material.color = Color.blue;
-
-    }
+        //plane.GetComponent<MeshRenderer>().material.color = Color.blue;
+        if (collision.CompareTag("Hammer"))
+        {
+            hammerTouching = false;
+            //plane.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+            
+        if (collision.CompareTag("Rock"))
+        {
+            rockTouching = false;
+            //plane.GetComponent<MeshRenderer>().enabled = false;
+        }
+    } 
 
 }
